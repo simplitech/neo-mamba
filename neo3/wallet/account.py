fddb3191ea568f48bf5c83d873236ac25a56f4e6
luc10921 <lucas.uezu@simpli.com.br>
2021-04-22 19:27:51 -0300

    Create account

--- neo3/wallet/account.py
+++ neo3/wallet/account.py
@@ -1,5 +1,7 @@
 from __future__ import annotations
 
+from typing import List
+
 from neo3 import contracts, settings, wallet
 from neo3.core import cryptography, types
 
@@ -10,7 +12,7 @@ class Account:
         self._script_hash: types.UInt160 = script_hash
         self._nep2key: str = nep2key
         self.label = ''
-        self.is_default = False
+        self.is_default = True
         self.lock = False
         self._key: cryptography.KeyPair = None
         self.contract: contracts.Contract = None
@@ -27,6 +29,34 @@ class Account:
     @property
     def has_key(self) -> bool:
         return self._nep2key is not None
+
+    @classmethod
+    def new_account(cls, passphrase: str) -> Account:
+        key_pair = cryptography.KeyPair.generate()
+        return cls.new_account_from_private_key(key_pair, passphrase)
+
+    @classmethod
+    def new_account_from_private_key(cls, key_pair: cryptography.KeyPair, passphrase: str) -> Account:
+        public_key = key_pair.public_key
+        from neo3.core import to_script_hash
+        script_hash = to_script_hash(public_key)
+        from neo3.wallet import private_key_to_nep2
+
+        account: Account
+        account = cls(script_hash, private_key_to_nep2(key_pair.private_key, passphrase))
+        account._key = key_pair
+        account.contract.script = public_key.get_veritification_script()
+        account.contract.parameter_list = Account.get_contract_params(1)
+
+        return account
+
+    @classmethod
+    def get_contract_params(cls, n: int) -> List[contracts.ContractParameterType]:
+        params: List[contracts.ContractParameterType] = []
+        for i in range(0, n):
+            params[i] = contracts.ContractParameterType.SIGNATURE
+
+        return params
 
     @classmethod
     def from_json(cls, json: dict) -> Account:
